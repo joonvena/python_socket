65b09e7ae7fbd21de9d4b5e390d96036567dfefd
joonvena <joonvena@gmail.com>
2019-10-03 11:23:32 +0300

    Added thread to take care of data fetching

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from client import get_sensordata
 import time
 import json
+import threading
 
-for i in range(0, 10):
-    convert_to_json = json.loads(get_sensordata())
-    print(convert_to_json['coordY'])
-    i += 1
-    time.sleep(5)
+
+def get_data():
+    while True:
+        convert_to_json = json.loads(get_sensordata())
+        return convert_to_json['coordY']
+        time.sleep(5)
+
+
+data_thread = threading.Thread(target=get_data)
+data_thread.daemon = True
+data_thread.start()
+
+
+while True:
+    if(get_data() == "test"):
+        print("Yes it is test")
